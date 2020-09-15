be6448c74b7d20d71435f1283d0465299097da3f
Philip (zixoan) <zixoan@protonmail.com>
2020-09-15 23:55:03 +0200

    Add basic random and round robin load balance strategy with tests

---
+++ Cuber.Server/Balancing/LoadBalanceStrategyBase.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+using Zixoan.Cuber.Server.Config;
+
+namespace Zixoan.Cuber.Server.Balancing
+{
+    public abstract class LoadBalanceStrategyBase : ILoadBalanceStrategy
+    {
+        protected readonly List<Target> targets;
+
+        protected LoadBalanceStrategyBase(List<Target> targets)
+        {
+            this.targets = targets;
+        }
+
+        public abstract Target GetTarget();
+    }
+}
---
+++ Cuber.Server/Balancing/RoundRobinLoadBalanceStrategy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+using Zixoan.Cuber.Server.Config;
+
+namespace Zixoan.Cuber.Server.Balancing
+{
+    public class RoundRobinLoadBalanceStrategy : LoadBalanceStrategyBase
+    {
+        private int currentTarget;
+
+        public RoundRobinLoadBalanceStrategy(List<Target> targets) 
+            : base(targets)
+        {
+        }
+
+        public override Target GetTarget()
+            => this.targets[currentTarget++ % this.targets.Count];
+    }
+}
