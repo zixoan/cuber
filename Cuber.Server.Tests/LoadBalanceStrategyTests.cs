5c1704dd113e835295d47903833f99b130505e24
Philip (zixoan) <zixoan@protonmail.com>
2020-10-13 23:59:42 +0200

    Add test for least connection strategy

--- Cuber.Server.Tests/LoadBalanceStrategyTests.cs
+++ Cuber.Server.Tests/LoadBalanceStrategyTests.cs
@@ -12,7 +12,8 @@ namespace Zixoan.Cuber.Server.Tests
         private static readonly List<Target> targets = new List<Target>
             {
                 new Target { Ip = "10.0.0.1", Port = 8080 },
-                new Target { Ip = "10.0.0.2", Port = 8081 }
+                new Target { Ip = "10.0.0.2", Port = 8081 },
+                new Target { Ip = "10.0.0.3", Port = 8082 }
             };
 
         [Fact]
@@ -22,8 +23,10 @@ namespace Zixoan.Cuber.Server.Tests
 
             Assert.Equal(targets[0], roundRobin.GetTarget());
             Assert.Equal(targets[1], roundRobin.GetTarget());
+            Assert.Equal(targets[2], roundRobin.GetTarget());
             Assert.Equal(targets[0], roundRobin.GetTarget());
             Assert.Equal(targets[1], roundRobin.GetTarget());
+            Assert.Equal(targets[2], roundRobin.GetTarget());
             Assert.Equal(targets[0], roundRobin.GetTarget());
         }
 
@@ -36,6 +39,35 @@ namespace Zixoan.Cuber.Server.Tests
             Assert.Contains(random.GetTarget(), targets);
             Assert.Contains(random.GetTarget(), targets);
             Assert.Contains(random.GetTarget(), targets);
+        }
+
+        [Fact]
+        public void TestLeastConnectionStrategy()
+        {
+            var leastConnection = new LeastConnectionLoadBalanceStrategy(targets);
+
+            targets[1].IncrementConnections();
+            targets[1].IncrementConnections();
+
+            Assert.Equal(targets[0], leastConnection.GetTarget());
+
+            targets[0].IncrementConnections();
+            targets[0].IncrementConnections();
+            targets[0].IncrementConnections();
+
+            Assert.Equal(targets[2], leastConnection.GetTarget());
+
+            targets[1].IncrementConnections();
+
+            targets[2].IncrementConnections();
+            targets[2].IncrementConnections();
+            targets[2].IncrementConnections();
+
+            Assert.Equal(targets[0], leastConnection.GetTarget());
+
+            targets[0].IncrementConnections();
+
+            Assert.Equal(targets[1], leastConnection.GetTarget());
         }
     }
 }
