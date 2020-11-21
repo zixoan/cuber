3415d075bcc2504814ffb522e92a091cbe24dce8
Zixoan <zixoan@protonmail.com>
2020-11-21 23:14:54 +0100

    Use for loop in round-robin test

--- Cuber.Server.Tests/LoadBalanceStrategyTests.cs
+++ Cuber.Server.Tests/LoadBalanceStrategyTests.cs
@@ -9,7 +9,7 @@ namespace Zixoan.Cuber.Server.Tests
 {
     public class LoadBalanceStrategyTests
     {
-        private static readonly List<Target> targets = new List<Target>
+        private static readonly List<Target> Targets = new List<Target>
             {
                 new Target { Ip = "10.0.0.1", Port = 8080 },
                 new Target { Ip = "10.0.0.2", Port = 8081 },
@@ -19,55 +19,52 @@ namespace Zixoan.Cuber.Server.Tests
         [Fact]
         public void TestRoundRobinStrategy()
         {
-            ILoadBalanceStrategy roundRobin = LoadBalanceStrategyFactory.Create(BalanceStrategy.RoundRobin, targets);
+            ILoadBalanceStrategy roundRobin = LoadBalanceStrategyFactory.Create(BalanceStrategy.RoundRobin, Targets);
 
-            Assert.Equal(targets[0], roundRobin.GetTarget());
-            Assert.Equal(targets[1], roundRobin.GetTarget());
-            Assert.Equal(targets[2], roundRobin.GetTarget());
-            Assert.Equal(targets[0], roundRobin.GetTarget());
-            Assert.Equal(targets[1], roundRobin.GetTarget());
-            Assert.Equal(targets[2], roundRobin.GetTarget());
-            Assert.Equal(targets[0], roundRobin.GetTarget());
+            for (int i = 0; i < 26; i++)
+            {
+                Assert.Equal(Targets[i % Targets.Count], roundRobin.GetTarget());
+            }
         }
 
         [Fact]
         public void TestRandomStrategy()
         {
-            ILoadBalanceStrategy random = LoadBalanceStrategyFactory.Create(BalanceStrategy.Random, targets);
+            ILoadBalanceStrategy random = LoadBalanceStrategyFactory.Create(BalanceStrategy.Random, Targets);
 
-            Assert.Contains(random.GetTarget(), targets);
-            Assert.Contains(random.GetTarget(), targets);
-            Assert.Contains(random.GetTarget(), targets);
-            Assert.Contains(random.GetTarget(), targets);
+            Assert.Contains(random.GetTarget(), Targets);
+            Assert.Contains(random.GetTarget(), Targets);
+            Assert.Contains(random.GetTarget(), Targets);
+            Assert.Contains(random.GetTarget(), Targets);
         }
 
         [Fact]
         public void TestLeastConnectionStrategy()
         {
-            ILoadBalanceStrategy leastConnection = new LeastConnectionLoadBalanceStrategy(targets);
+            ILoadBalanceStrategy leastConnection = new LeastConnectionLoadBalanceStrategy(Targets);
 
-            targets[1].IncrementConnections();
-            targets[1].IncrementConnections();
+            Targets[1].IncrementConnections();
+            Targets[1].IncrementConnections();
 
-            Assert.Equal(targets[0], leastConnection.GetTarget());
+            Assert.Equal(Targets[0], leastConnection.GetTarget());
 
-            targets[0].IncrementConnections();
-            targets[0].IncrementConnections();
-            targets[0].IncrementConnections();
+            Targets[0].IncrementConnections();
+            Targets[0].IncrementConnections();
+            Targets[0].IncrementConnections();
 
-            Assert.Equal(targets[2], leastConnection.GetTarget());
+            Assert.Equal(Targets[2], leastConnection.GetTarget());
 
-            targets[1].IncrementConnections();
+            Targets[1].IncrementConnections();
 
-            targets[2].IncrementConnections();
-            targets[2].IncrementConnections();
-            targets[2].IncrementConnections();
+            Targets[2].IncrementConnections();
+            Targets[2].IncrementConnections();
+            Targets[2].IncrementConnections();
 
-            Assert.Equal(targets[0], leastConnection.GetTarget());
+            Assert.Equal(Targets[0], leastConnection.GetTarget());
 
-            targets[0].IncrementConnections();
+            Targets[0].IncrementConnections();
 
-            Assert.Equal(targets[1], leastConnection.GetTarget());
+            Assert.Equal(Targets[1], leastConnection.GetTarget());
         }
     }
 }
