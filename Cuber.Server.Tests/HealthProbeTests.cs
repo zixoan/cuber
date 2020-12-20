6aa7609552a43e5daaa81e4fd5c024bf23c1f36e
Philip (zixoan) <zixoan@protonmail.com>
2020-12-20 23:31:06 +0100

    Add HTTP health probe implementation and tests for it

--- Cuber.Server.Tests/HealthProbeTests.cs
+++ Cuber.Server.Tests/HealthProbeTests.cs
@@ -1,4 +1,4 @@
-using System.Net;
+﻿using System.Net;
 using System.Net.Sockets;
 using System.Threading.Tasks;
 
@@ -41,6 +41,38 @@ namespace Zixoan.Cuber.Server.Tests
             Target target = new Target { Ip = IPAddress.Loopback.ToString(), Port = 0 };
 
             Assert.False(await tcpProbe.IsReachable(target));
+        }
+
+        [Fact]
+        public async Task HttpHealthProbeSuccessful()
+        {
+            IHealthProbe httpProbe = new HttpHealthProbe(CuberOptions);
+
+            int port = PortHelper.GetFreePort();
+
+            HttpListener httpListener = new HttpListener();
+            httpListener.Prefixes.Add($"http://127.0.0.1:{port}/");
+            httpListener.Start();
+
+            Target target = new Target { Ip = IPAddress.Loopback.ToString(), Port = port };
+
+            Task<bool> reachableTask = httpProbe.IsReachable(target);
+
+            HttpListenerContext client = await httpListener.GetContextAsync();
+            client.Response.StatusCode = (int)HttpStatusCode.OK;
+            client.Response.Close();
+
+            Assert.True(await reachableTask);
+        }
+
+        [Fact]
+        public async Task HttpHealthProbeUnsuccessful()
+        {
+            IHealthProbe httpProbe = new HttpHealthProbe(CuberOptions);
+
+            Target target = new Target { Ip = IPAddress.Loopback.ToString(), Port = 0 };
+
+            Assert.False(await httpProbe.IsReachable(target));
         }
     }
 }
---
+++ Cuber.Server/Probe/HttpHealthProbe.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Options;
+
+using Zixoan.Cuber.Server.Config;
+
+namespace Zixoan.Cuber.Server.Probe
+{
+    public class HttpHealthProbe : IHealthProbe
+    {
+        private const int DefaultTimeout = 5000;
+
+        private readonly CuberOptions cuberOptions;
+        private readonly HttpClient httpClient;
+
+        public HttpHealthProbe(IOptions<CuberOptions> options)
+        {
+            this.cuberOptions = options.Value;
+            TimeSpan timeout = TimeSpan.FromMilliseconds(this.cuberOptions.HealthProbe.Timeout ?? DefaultTimeout);
+            this.httpClient = new HttpClient { Timeout = timeout };
+        }
+
+        public async Task<bool> IsReachable(Target target)
+        {
+            try
+            {
+                UriBuilder uriBuilder = new UriBuilder(
+                    "http",
+                    target.Ip,
+                    this.cuberOptions.HealthProbe.Port ?? target.Port,
+                    this.cuberOptions.HealthProbe.Path
+                );
+
+                HttpResponseMessage response = await httpClient.GetAsync(uriBuilder.Uri);
+
+                return response.IsSuccessStatusCode;
+            }
+            catch (Exception exception) when (exception is ArgumentException || exception is HttpRequestException)
+            {
+                return false;
+            }
+        }
+    }
+}
