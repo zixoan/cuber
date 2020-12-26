5e7dcd487317af5b72e597c394c00c9f0f965e48
Philip (zixoan) <zixoan@protonmail.com>
2020-12-26 20:39:01 +0100

    Add mathematical floor modulus int extension with tests

---
+++ Cuber.Server/Extensions/IntExtensions.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Zixoan.Cuber.Server.Extensions
+{
+    public static class IntExtensions
+    {
+        /// <summary>
+        /// Returns the mathematical floor modulus, where the result has the same sign as the divisor <paramref name="m"/>.
+        /// </summary>
+        /// <param name="this">The current value as dividend.</param>
+        /// <param name="m">The divisor.</param>
+        /// <returns>The floor modulus of the given dividend and divisor.</returns>
+        public static int FloorMod(this int @this, int m)
+            => @this - (int)Math.Floor((double)@this / m) * m;
+    }
+}
